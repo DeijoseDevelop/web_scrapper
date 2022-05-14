26077fe1a8dd13cecdf8ef144f6e1748e870cbb3
Deiver Vazquez <dvazquez@lsv-tech.com>
2022-05-13 19:41:35 -0500

    create functions to extract data of websites

---
+++ scrapper.py
@@ -0,0 +1,61 @@
+# selenium 4
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.support.relative_locator import locate_with
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from pprint import pprint
+
+
+variable = input('¿Que vas a investigar? >> ')
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+driver.get('http://www.google.com')
+driver.find_element(By.TAG_NAME, 'input').send_keys(variable + Keys.ENTER)
+
+ALL_RSO = WebDriverWait(driver, 30).until(EC.presence_of_all_elements_located((By.ID, "rso")))
+
+
+def validate_links(link):
+    """
+    this function is in charge of making the validations to filter the links.
+    """
+    if link.text != '' and not 'google' in str(link.get_attribute('href')):
+        return True
+
+def save_links():
+    """
+    this function is in charge
+    of searching all the a tags inside all the divs with id 'rso'
+    and save all the filtered href attributes inside a list
+    and return the list.
+    """
+    data = []
+    links = ''
+    for i in ALL_RSO:
+        links = i.find_elements(By.TAG_NAME, 'a')
+        data = [j.get_attribute('href') for j in links if validate_links(j)]
+    return data
+
+def extract_data(n):
+    """
+    this function extracts data from websites and stores it in .docx files.
+    """
+    content = driver.find_element(By.TAG_NAME, 'main').text
+    with (open('save/archive_{}.docx'.format(n), 'w')) as FILE:
+        FILE.write(content)
+
+def search_links():
+    """
+    this function is in charge of searching to enter in each one of the links
+    """
+    links = save_links()
+    for i in range(len(links) + 1):
+        driver.get(links[i])
+        extract_data(i + 1)
+        time.sleep(2)
+
+search_links()
